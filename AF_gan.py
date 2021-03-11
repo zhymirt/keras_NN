2b14132900d5ce80a89a6c053da8ef3909a8fc53
Zhymir Thompson <dzhymir@gmail.com>
2021-03-11 14:58:59 -0500

    reset and readded files

--- AF_gan.py
+++ AF_gan.py
@@ -1,16 +1,19 @@
-import numpy as np
-from tensorflow.python.keras.backend import sign
-from keras_gan import GAN
-from keras_data import data_to_dataset, plot_data
 import os
-import tensorflow as tf
 
-from custom_losses import GeneratorWassersteinLoss, DiscriminatorWassersteinLoss
+import numpy as np
+import tensorflow as tf
+from matplotlib import pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.python.keras.backend import sign
 
+from custom_losses import (DiscriminatorWassersteinLoss,
+                           GeneratorWassersteinLoss)
+from keras_data import data_to_dataset, plot_data
+from keras_gan import GAN
 
 keys = ['time', 'accel', 'intaccel', 'sg1', 'sg2', 'sg3']
+save_paths = list(map(lambda x: 'time_' + x, keys[1:]))
 
 def read_file(filename):
     """ Read file at given filename and return dictionary of lists composed of floats from file"""
@@ -24,14 +27,21 @@ def read_file(filename):
             split = data_line.split()
             for key, value in zip(keys, split):
                 data[key].append(float(value))
-            # data['time'].append(float(split[0]))
-            # data['accel'].append(float(split[1]))
-            # data['intaccel'].append(float(split[2]))
-            # data['sg1'].append(float(split[3]))
-            # data['sg2'].append(float(split[4]))
-            # data['sg3'].append(float(split[5]))
             data_line = tempFile.readline()
     return data
+
+def read_file_to_arrays(filename):
+    time, data = list(), list()
+    with open(filename, 'r') as tempFile:
+        tempFile.readline()
+        tempFile.readline()
+        data_line = tempFile.readline()
+        while data_line:
+            split = data_line.split()
+            time.append(float(split[0])) # append time to time column
+            data.append([float(value) for value in split[1:]]) # append data as 5-tuple
+            data_line = tempFile.readline()
+    return time, data
 
 def signal_dict_to_list(data):
     """ Take signal dictionary and convert to list, time excluded"""
@@ -40,21 +50,29 @@ def signal_dict_to_list(data):
     signal_data = list(map(lambda x: [data[key][x] for key in keys[1:]], indices))
     return signal_data # grab all but time list and make list
 
+# def convert_to_time_data
+
+def plot_data(x_values, list_y_values, show=False, save=True, save_path=''):
+    for idx in range(len(save_paths)):
+        y_values = list_y_values[idx]
+        print(y_values)
+        print(y_values.shape)
+        plt.plot(x_values, y_values)
+        plt.title(label=save_path.split('/')[-1]+save_paths[idx])
+        if save and save_path:
+            plt.savefig(save_path+save_paths[idx])
+        if show:
+            plt.show()
+        plt.close()
 
 if __name__ == '__main__':
     data_type, batch_size = 'float32', 1
     # print(os.getcwd())
     # exit()
-    signals = [ read_file(os.path.join('../signal_data', name)) for name in ['T04.txt',
+    time, benign_data = read_file_to_arrays('./signal_data/T04.txt')[0], [ read_file_to_arrays(os.path.join('./signal_data', name))[1] for name in ['T04.txt',
         'T04repeat.txt', 'T05.txt', 'T06.txt', 'T07.txt', 'T08.txt']]
-    # signals = list(map(read_file, signals))
-    # for signal in signals:
-    #     print(len(signal['time']))
-    data_size, num_data_types = len(signals[0]['time']), len(keys) - 1
+    data_size, num_data_types = len(time), len(keys) -1
     vector_size, latent_dimension = data_size, 164
-    benign_data = list(map(signal_dict_to_list, signals))
-    # print('Length of data: {}, Number of data points: {}'.format(len(benign_data), len(benign_data[0])))
-    # print(np.array(benign_data).shape)
     dataset = data_to_dataset(benign_data, dtype=data_type, batch_size=batch_size, shuffle=True)
     # create discriminator and generator
     discriminator = keras.Sequential(
@@ -90,14 +108,17 @@ if __name__ == '__main__':
             layers.LeakyReLU(alpha=0.2),
             layers.Conv2D(1, (num_data_types, num_data_types), padding="same", activation="sigmoid"),
             layers.Cropping2D(((0, 0), (0, 1))),
+            # layers.Reshape((data_size, num_data_types)),
+            layers.Flatten(),
+            layers.Dense(1000, activation='relu'),
+            layers.Dense(500, activation=tf.math.cos),
+            layers.Dense(data_size * num_data_types, activation='tanh'),
             layers.Reshape((data_size, num_data_types))
         ],
         name="generator",
     )
     generator.add
     generator.summary()
-    # print(np.array(benign_data).shape)
-    # exit()
     # generator attempts to produce even numbers, discriminator will tell if true or not
     gan = GAN(discriminator=discriminator, generator=generator, latent_dim=latent_dimension)
     gan.compile(d_optimizer=keras.optimizers.Adam(learning_rate=0.0003),
@@ -108,5 +129,8 @@ if __name__ == '__main__':
                 d_loss_fn=DiscriminatorWassersteinLoss()
     )
     gan.fit(dataset, epochs=50)
-    time = np.array(signals[0]['time'])
-    plot_data(time, )
+    # generator.save('af_generator.h5')
+    # discriminator.save('af_discriminator.h5')
+    # time = np.array(signals[0]['time'])
+    prediction = generator.predict(tf.random.normal(shape=(1, latent_dimension)))[0]
+    plot_data(time, np.transpose(np.array(prediction), (1, 0)) , show=True, save=True, save_path='./keras_NN/results/AF_v1_')
--- sine_gan.py
+++ sine_gan.py
@@ -1,17 +1,23 @@
+import os
 from random import randint
-
+from math import sqrt
+from statistics import mean # , pstdev
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
 from matplotlib import pyplot as plt
 from tensorflow.keras import layers
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.python.keras.layers.advanced_activations import LeakyReLU
-from tensorflow.python.keras.layers.pooling import GlobalMaxPool1D
+from tensorflow.python.keras.layers.core import Dense
+from tensorflow.python.keras.layers.normalization_v2 import BatchNormalization
+from tensorflow.python.keras.layers.pooling import GlobalAveragePooling1D, GlobalMaxPool1D
+from keras_model_functions import plot_recurrence
 
 from custom_losses import (DiscriminatorWassersteinLoss,
                            GeneratorWassersteinLoss)
 from keras_data import data_to_dataset, plot_data
-from keras_gan import GAN
+from keras_gan import GAN, WGAN
 
 
 start_point, end_point, vector_size = 0, 2, 1000
@@ -21,65 +27,166 @@ def generate_sine(start, end, points, amplitude=1, frequency=1):
     signal = amplitude*np.sin(2*np.pi*frequency*time)
     return signal
 
-def plot_sine(data, show=False, save=False, save_path=''):
-    plot_data(np.linspace(start_point, end_point, num=vector_size), data, show=show, save=save, save_path=save_path)
+def plot_sine(data, show=False, save=False, save_path='', plot_trend=False, trend_signal=None):
+    plot_data(np.linspace(start_point, end_point, num=vector_size),
+     data, trend_signal if plot_trend and trend_signal is not None and len(trend_signal) > 0 else None,
+      show=show, save=save, save_path=save_path)
+
+def generate_image_summary(generator, latent_dim_size, num_rand_images=1, plot_trend=False, trend_signal=None, show=False, save=False, save_dir='./', save_desc=''):
+    plot_sine(generator.predict(tf.zeros(shape=(1, latent_dim_size)))[0], show=show, save=save, save_path=os.path.join( save_dir, '/sine_zeros + save_desc'),
+     plot_trend=plot_trend, trend_signal=trend_signal)
+    for idx in range(num_rand_images):
+        plot_sine(generator.predict(tf.random.normal(shape=(1, latent_dim_size)))[0], show=True, save=save, save_path=os.path.join( save_dir, 'sine_norm' + save_desc + '_' + str(idx)),
+         plot_trend=plot_trend, trend_signal=trend_signal)
+    plot_sine(generator.predict(tf.ones(shape=(1, latent_dim_size)))[0], show=show, save=save, save_path=os.path.join( save_dir, 'sine_ones' + save_desc),
+     plot_trend=plot_trend, trend_signal=trend_signal)
+
+
+# def mean(vector):
+#     return sum(vector)/len(vector) if vector else 0
+
+def std_dev(vector):
+    if vector is None or len(vector) == 0:
+        return 0
+    size = len(vector)
+    avg = sum(vector)/size
+    return sqrt(sum(list(map(lambda x: (x - avg)**2, vector)))/size)
+
+def standardize(vector):
+    avg = mean(vector)
+    deviation = std_dev(vector)
+    return list(map(lambda x: (x - avg / deviation) if deviation != 0 else 0, vector))
+
 
 if __name__ == '__main__':
-    latent_dimension, variations, data_size, batch_size, data_type = 128, 100, 1e4, 64, 'float32'
-    benign_data = [generate_sine(start_point, end_point, vector_size, frequency=randint(1, 3)) for _ in range(int(data_size))] # generate 100 points of sine wave
-    for idx in range(4):
-        plot_sine(benign_data[idx], show=False)
+    linux = False
+    if linux:
+        from tensorflow.compat.v1 import ConfigProto
+        from tensorflow.compat.v1 import InteractiveSession
+
+        config = ConfigProto()
+        config.gpu_options.allow_growth = True
+        session = InteractiveSession(config=config)
+    latent_dimension, epochs, data_size, batch_size, data_type = 256, 2, int(1e2), 2, 'float32'
+    save_desc = '_{}{}{}{}{}{}{}{}{}{}'.format('latent_dimension_', latent_dimension, '_epochs_', epochs, '_data_size_', data_size, '_batch_size_', batch_size, '_type_', 'cnn_fc')
+    early_stop = EarlyStopping(monitor='g_loss', mode='min', verbose=1, patience=3)
+    checkpoint = ModelCheckpoint(filepath='./tmp/checkpoint', save_weights_only=True)
+    callback_list = [checkpoint] # [early_stop, checkpoint]
+    benign_data = [generate_sine(start_point, end_point, vector_size, frequency=1) for _ in range(int(data_size))] # generate 100 points of sine wave
+    # for idx in range(2):
+    #     plot_sine(benign_data[idx], show=True)
     dataset = data_to_dataset(benign_data, dtype=data_type, batch_size=batch_size, shuffle=True)
     # create discriminator and generator
     discrim = keras.Sequential(
     [
         layers.Reshape((vector_size, 1,), input_shape=(vector_size,)),
-        layers.Conv1D(64, (3), strides=(3), padding="same"),
+        layers.Conv1D(256, (5), strides=(2), padding="same"),
+        layers.BatchNormalization(),
         layers.LeakyReLU(alpha=0.2),
-        layers.Conv1D(128, (3), strides=(3), padding="same"),
+        layers.Conv1D(256, (3), strides=(2), padding="same"),
+        layers.BatchNormalization(),
         layers.LeakyReLU(alpha=0.2),
-        layers.GlobalMaxPooling1D(),
-        layers.Reshape((128,)),
-        layers.Dense(1, activation='sigmoid'),
+        layers.Conv1D(256, (3), strides=2, padding="same"),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(alpha=0.2),
+        layers.GlobalAveragePooling1D(),
+        # layers.GlobalMaxPooling1D(),
+        layers.Flatten(),
+        # layers.Dense(3000),
+        # layers.LeakyReLU(alpha=0.2),
+        # layers.Dense(2000),
+        # layers.LeakyReLU(alpha=0.2),
+        layers.Dense(1) #, activation='sigmoid'),
     ],
     name="discriminator",
     )
-    # print(discrim.input_shape, ' ', discrim.output_shape)
+    discrim = keras.Sequential([
+        layers.Dense(vector_size*4, input_shape=(vector_size,)),
+        layers.Dense(1024),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Dense(1024),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Dense(1024),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Dense(1024),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Dense(1)
+    ],
+    name="discriminator",
+    )
+    discrim.summary()
     # exit()
     generator = keras.Sequential(
         [
-            layers.Dense(int(vector_size * 0.25) * latent_dimension, input_shape=(latent_dimension,)),
+            # layers.Dense(vector_size * latent_dimension, input_shape=(latent_dimension,)),
+            # layers.LeakyReLU(alpha=0.2),
+            # layers.Dense(latent_dimension),
+            # layers.LeakyReLU(alpha=0.2),
+            layers.Reshape((latent_dimension, 1), input_shape=(latent_dimension,)),
+            layers.Conv1DTranspose(256, 3, strides=2, padding='same'),
+            layers.BatchNormalization(),
             layers.LeakyReLU(alpha=0.2),
-            layers.Reshape((-1, latent_dimension)),
-            layers.Conv1DTranspose(128, 7, strides=2, padding='same'),
+            layers.Conv1DTranspose(256, 3, strides=2, padding='same'),
+            layers.BatchNormalization(),
             layers.LeakyReLU(alpha=0.2),
-            layers.Conv1DTranspose(256, 7, strides=2, padding='same'),
-            layers.LeakyReLU(alpha=0.2),
-            layers.Conv1D(1, (vector_size), padding='same', activation='sigmoid'),
-            layers.Reshape((vector_size,))
+            layers.Conv1D(1, (3), padding='same', activation='tanh'),
+            layers.BatchNormalization(),
+            # layers.Reshape((vector_size,)),
             # layers.Reshape((4 * vector_size,)),
-        #     layers.Dense(300),
-        #     layers.Dense(200, activation=tf.math.cos),
-        #     layers.Dense(vector_size, activation='tanh')
+            layers.Flatten(),
+            # layers.Dense(4000, activation='sigmoid'),
+
+            # layers.Dense(2000, activation='relu'),
+            # layers.Dense(vector_size, activation='relu')
+            layers.Dense(vector_size, activation=tf.cos),
+            layers.BatchNormalization(),
+            layers.Dense(vector_size, activation='tanh')
         ],
         name="generator",
     )
-    print(generator.input_shape, ' ', generator.output_shape)
+    generator = keras.Sequential(
+        [
+            layers.Dense(vector_size * latent_dimension, input_shape=(latent_dimension,)),
+            layers.Dense(1024),
+            layers.BatchNormalization(),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Dense(1024),
+            layers.BatchNormalization(),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Dense(1024),
+            layers.BatchNormalization(),
+            layers.Dense(1024, activation=tf.cos),
+            layers.Dense(vector_size, activation='tanh')
+        ],
+        name="generator",
+    )
     generator.summary()
+    # d_optimizer = keras.optimizers.Adam(learning_rate=0.0003)
     # exit()
     # generator attempts to produce even numbers, discriminator will tell if true or not
-    gan = GAN(discriminator=discrim, generator=generator, latent_dim=latent_dimension)
-    gan.compile(d_optimizer=keras.optimizers.Adam(learning_rate=0.0003),
-                g_optimizer=keras.optimizers.Adam(learning_rate=0.0003),
+    wgan = WGAN(discriminator=discrim, generator=generator, latent_dim=latent_dimension)
+    wgan.compile(d_optimizer=keras.optimizers.Adam(learning_rate=0.0006),
+                g_optimizer=keras.optimizers.Adam(learning_rate=0.0006),
                 # g_loss_fn=keras.losses.BinaryCrossentropy(from_logits=True),
                 # d_loss_fn=keras.losses.BinaryCrossentropy(from_logits=True)
                 g_loss_fn=GeneratorWassersteinLoss(),
                 d_loss_fn=DiscriminatorWassersteinLoss()
     )
-    gan.fit(dataset, epochs=50)
+    wgan.set_train_epochs(4, 1)
+    wgan.fit(dataset, epochs=epochs, callbacks=callback_list)
     generator.save('sine_generator.h5')
     discrim.save('sine_discriminator.h5')
-    plot_sine(generator.predict(tf.zeros(shape=(1, latent_dimension)))[0], show=True, save=True, save_path='./results/sine_zeros')
-    for idx in range(3):
-        plot_sine(generator.predict(tf.random.normal(shape=(1, latent_dimension)))[0], show=True, save=True, save_path='./results/sine_norm'+str(idx))
-    plot_sine(generator.predict(tf.ones(shape=(1, latent_dimension)))[0], show=True, save=True, save_path='./results/sine_ones')
+    from pyts.image import RecurrencePlot
+    rp, trend = RecurrencePlot(threshold='point', percentage=20), generate_sine(start_point, end_point, vector_size, amplitude=1, frequency=1)
+    plot_recurrence(np.linspace(start_point, end_point, vector_size), generator.predict(tf.zeros(shape=(1, latent_dimension)))[0], rp, show=True, save=False)
+    generate_image_summary(generator=generator, latent_dim_size=latent_dimension, num_rand_images=3, show=True, save=False,
+     save_dir='./results', save_desc=save_desc, plot_trend=True, trend_signal=trend)
+    # plot_sine(generator.predict(tf.zeros(shape=(1, latent_dimension)))[0], show=True, save=False, save_path='./results/sine_zeros' + save_desc)
+    # plot_sine(standardize(generator.predict(tf.zeros(shape=(1, latent_dimension)))[0]), show=True, save=False)
+    # for idx in range(3):
+    #     plot_sine(generator.predict(tf.random.normal(shape=(1, latent_dimension)))[0], show=True, save=False, save_path='./results/sine_norm' + save_desc + '_' + str(idx))
+    # plot_sine(generator.predict(tf.ones(shape=(1, latent_dimension)))[0], show=True, save=False, save_path='./results/sine_ones' + save_desc)
