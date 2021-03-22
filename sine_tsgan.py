17cf3a0c4e43b248eef6708725cd9f4e7d24adee
zhymir thompson <dzhymir@gmail.com>
2021-03-22 14:25:17 -0400

    started conditional for generator, might not work this way

--- sine_tsgan.py
+++ sine_tsgan.py
@@ -28,13 +28,13 @@ import matplotlib.pyplot as plt
 
 if __name__=='__main__':
     vector_size = 100
-    latent_dimension, epochs, data_size, batch_size, data_type = 256, 2, int(1e2), 1, 'float32'
+    latent_dimension, epochs, data_size, batch_size, data_type = 256, 16, int(1e2), 1, 'float32'
     save_desc = '_{}{}{}{}{}{}{}{}{}{}'.format('latent_dimension_', latent_dimension, '_epochs_', epochs, '_data_size_', data_size, '_batch_size_', batch_size, '_type_', 'cnn_fc')
     early_stop = EarlyStopping(monitor='g_loss', mode='min', verbose=1, patience=3)
     checkpoint = ModelCheckpoint(filepath='./tmp/checkpoint', save_weights_only=True)
     callback_list = [checkpoint] # [early_stop, checkpoint]
     benign_data = [generate_sine(start_point, end_point, vector_size, frequency=15) for _ in range(int(data_size))] # generate 100 points of sine wave
-    spectrograms = list(map(lambda x: plt.specgram(x)[0], benign_data))
+    # spectrograms = list(map(lambda x: plt.specgram(x)[0], benign_data))
     spectrogram_scipy = list(map(lambda x: signal.spectrogram(x, fs=1)[2], benign_data))
     # plt.pcolormesh()
     # plt.close()
@@ -42,9 +42,12 @@ if __name__=='__main__':
     # plt.show()
     # plt.specgram(benign_data[0], Fs=1)
     # plt.show()
-    f, t, Sxx = signal.spectrogram(benign_data[0], 50)
-    # plt.figure()
-    # plt.imshow(Sxx)
+    plt.close()
+    f, t, Sxx = signal.spectrogram(benign_data[0])
+    # plt.figure(figsize=(5, 4))
+    # plt.imshow(Sxx, aspect='auto')
+    # plt.tight_layout()
+    # plt.show()
     # plt.close()
 
     # img_data = plt.pcolormesh(t, f, Sxx, shading='gouraud').get_array()
@@ -54,59 +57,114 @@ if __name__=='__main__':
     # plt.ylim((0, 13))
     # plt.show()
     print("Spectrogram Shape: {}\nBenign data Shape: {}".format(spectrogram_scipy[0].shape, benign_data[0].shape))
-    spectrogram_dataset = tf.data.Dataset.from_tensor_slices(spectrogram_scipy)
+    # spectrogram_dataset = tf.data.Dataset.from_tensor_slices(spectrogram_scipy)
+    spectrogram_dataset = data_to_dataset(spectrogram_scipy, dtype=data_type, batch_size=batch_size)
     dataset = data_to_dataset(benign_data, dtype=data_type, batch_size=batch_size, shuffle=True)
     # exit()
-    image_shape = (129, 26,)
+    print(spectrogram_scipy[0])
+    image_shape = (51, 1)
     discriminator_1 = keras.Sequential([
-        layers.Reshape((129, 26, 1,), input_shape=image_shape),
-        layers.Conv2D(64, 5),
-        layers.Conv2D(64, 3),
+        layers.Reshape((1,)+image_shape, input_shape=image_shape),
+        layers.Conv1D(64, 5),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv1D(64, 3),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv1D(64, 3),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv1D(1, 3),
+        layers.LeakyReLU(alpha=0.2),
         layers.Flatten(),
-        layers.Dense(1024),
+        layers.Dense(32),
         layers.Dense(1)
     ], name='discriminator_1')
-    discriminator_2 = keras.Sequential([
-        layers.Reshape((vector_size, 1,), input_shape=(vector_size,)),
-        layers.Conv1D(64, 5, strides=3),
-        layers.Conv1D(64, 3, strides=2),
-        layers.Flatten(),
-        layers.Dense(1024),
-        layers.Dense(1)
-    ], name='discriminator_2')
+
+    discriminator_2_spec_input = layers.Input(shape=image_shape)
+    discriminator_2_spec = layers.Embedding(10, 20)(discriminator_2_spec_input)
+    discriminator_2_spec = layers.Dense(vector_size)(discriminator_2_spec)
+    discriminator_2_spec = layers.Reshape((vector_size, 1))(discriminator_2_spec)
+
+    discriminator_2_vector_input = layers.Input(shape=(vector_size,))
+    discriminator_2_vector = layers.Reshape(vector_size, 1)(discriminator_2_vector_input)
+
+    discriminator_2 = layers.Concatenate()([discriminator_2_vector, discriminator_2_spec])
+    discriminator_2 = layers.Conv1D(64, 5, strides=3)(discriminator_2)
+    discriminator_2 = layers.LeakyReLU(alpha=0.2)(discriminator_2)
+    discriminator_2 = layers.Conv1D(64, 3, strides=2)(discriminator_2)
+    discriminator_2 = layers.LeakyReLU(alpha=0.2)(discriminator_2)
+    discriminator_2 = layers.Conv1D(1, 3)(discriminator_2)
+    discriminator_2 = layers.LeakyReLU(alpha=0.2)(discriminator_2)
+    discriminator_2 = layers.Flatten()(discriminator_2)
+    discriminator_2 = layers.Dense(32)(discriminator_2)
+    discriminator_2 = layers.Dense(1)(discriminator_2)
+    discriminator_2 = keras.Model(inputs=(discriminator_2_vector_input, discriminator_2_spec_input), outputs=discriminator_2, name="discriminator_2")
+    # discriminator_2 = keras.Sequential([
+    #     layers.Reshape((vector_size, 1,), input_shape=(vector_size,)),
+    #     layers.Conv1D(64, 5, strides=3),
+    #     layers.LeakyReLU(alpha=0.2),
+    #     layers.Conv1D(64, 3, strides=2),
+    #     layers.LeakyReLU(alpha=0.2),
+    #     layers.Conv1D(1, 3),
+    #     layers.LeakyReLU(alpha=0.2),
+    #     layers.Flatten(),
+    #     layers.Dense(32),
+    #     layers.Dense(1)
+    # ], name='discriminator_2')
     generator_1 = keras.Sequential([
-        layers.Dense(32 * 7, input_shape=(latent_dimension,)),
+        layers.Dense(12, input_shape=(latent_dimension,)),
         layers.LeakyReLU(alpha=0.2),
-        layers.Reshape((32, 7, 1,)),
-        layers.Conv2DTranspose(64, (3, 2), strides=(2, 2)),
+        layers.Reshape((12, 1,)),
+        layers.Conv1DTranspose(64, 3, strides=2),
         layers.BatchNormalization(),
         layers.LeakyReLU(alpha=0.2),
-        layers.Conv2DTranspose(64, (3, 2), strides=(2, 2)),
+        layers.Conv1DTranspose(1, 3, strides=2),
         layers.BatchNormalization(),
         layers.LeakyReLU(alpha=0.2),
-        layers.Conv2D(1, 3),
-        layers.LeakyReLU(alpha=0.2),
-        layers.Reshape((129, 26,)),
+        # layers.Conv2D(1, 3),
+        # layers.LeakyReLU(alpha=0.2),
+        layers.Reshape(image_shape),
     ], name='generator_1')
-    generator_2 = keras.Sequential([
-        layers.Reshape((129, 26, 1,), input_shape=image_shape),
-        layers.Conv2D(16, (3, 2), strides=(2, 2)),
-        layers.Conv2D(16, (3, 2), strides=(2, 2)),
-        layers.Flatten(),
-        layers.Dense(64),
-        layers.LeakyReLU(alpha=0.2),
-        layers.Reshape((64, 1)),
-        layers.Conv1DTranspose(16, 3, strides=3, dtype=data_type),
-        layers.BatchNormalization(),
-        layers.LeakyReLU(alpha=0.2, dtype=data_type),
-        layers.Conv1DTranspose(16, 3, strides=3, dtype=data_type),
-        layers.BatchNormalization(),
-        layers.LeakyReLU(alpha=0.2, dtype=data_type),
-        layers.Conv1D(1, 3, dtype=data_type),
-        layers.Flatten(),
-        layers.Dense(32, activation=tf.cos, dtype=data_type),
-        layers.Dense(vector_size, activation='tanh', dtype=data_type)
-    ], name='generator_2')
+
+    generator_2_spec_input = layers.Input(shape=image_shape)
+    generator_2_spec = layers.Embedding(10, 20)(generator_2_spec_input)
+    generator_2_spec = layers.Dense()
+    generator_2 = layers.Conv1D(16, 3, strides=2)(generator_2)
+    generator_2 = layers.Conv1D(16, 3, strides=2)(generator_2)
+    generator_2 = layers.Conv1D(1, 3)(generator_2)
+    generator_2 = layers.Flatten()(generator_2)
+    generator_2 = layers.Dense(64)(generator_2)
+    generator_2 = layers.LeakyReLU(alpha=0.2)(generator_2)
+    generator_2 = layers.Reshape((64, 1))(generator_2)
+    generator_2 = layers.Conv1DTranspose(16, 3, strides=3, dtype=data_type)(generator_2)
+    generator_2 = layers.BatchNormalization()(generator_2)
+    generator_2 = layers.LeakyReLU(alpha=0.2, dtype=data_type)(generator_2)
+    generator_2 = layers.Conv1DTranspose(16, 3, strides=3, dtype=data_type)(generator_2)
+    generator_2 = layers.BatchNormalization()(generator_2)
+    generator_2 = layers.LeakyReLU(alpha=0.2, dtype=data_type)(generator_2)
+    generator_2 = layers.Conv1D(1, 3, dtype=data_type)(generator_2)
+    generator_2 = layers.Flatten()(generator_2)
+    generator_2 = layers.Dense(32, activation=tf.cos, dtype=data_type)(generator_2)
+    generator_2 = layers.Dense(vector_size, activation='tanh', dtype=data_type)(generator_2)
+    generator_2 = keras.Model(inputs=(), outputs=, name="generator_2")
+    # generator_2 = keras.Sequential([
+    #     layers.Reshape((1,)+image_shape, input_shape=image_shape),
+    #     layers.Conv1D(16, 3, strides=2),
+    #     layers.Conv1D(16, 3, strides=2),
+    #     layers.Conv1D(1, 3),
+    #     layers.Flatten(),
+    #     layers.Dense(64),
+    #     layers.LeakyReLU(alpha=0.2),
+    #     layers.Reshape((64, 1)),
+    #     layers.Conv1DTranspose(16, 3, strides=3, dtype=data_type),
+    #     layers.BatchNormalization(),
+    #     layers.LeakyReLU(alpha=0.2, dtype=data_type),
+    #     layers.Conv1DTranspose(16, 3, strides=3, dtype=data_type),
+    #     layers.BatchNormalization(),
+    #     layers.LeakyReLU(alpha=0.2, dtype=data_type),
+    #     layers.Conv1D(1, 3, dtype=data_type),
+    #     layers.Flatten(),
+    #     layers.Dense(32, activation=tf.cos, dtype=data_type),
+    #     layers.Dense(vector_size, activation='tanh', dtype=data_type)
+    # ], name='generator_2')
     print(discriminator_1.input_shape)
     print(discriminator_2.input_shape)
     print(generator_1.input_shape)
@@ -135,5 +193,6 @@ if __name__=='__main__':
                 g_loss_fn=GeneratorWassersteinLoss(),
                 d_loss_fn=DiscriminatorWassersteinLoss()
     )
-    spectrogram_wgan.fit(dataset, epochs=epochs, batch_size=batch_size, callbacks=callback_list)
-    # sine_wave_wgan.fit(dataset, epochs=epochs, batch_size=batch_size, callbacks=callback_list)
+    spectrogram_wgan.set_train_epochs(5, 1)
+    spectrogram_wgan.fit(spectrogram_dataset, epochs=epochs, batch_size=batch_size, callbacks=callback_list)
+    sine_wave_wgan.fit(dataset, epochs=epochs, batch_size=batch_size, callbacks=callback_list)
