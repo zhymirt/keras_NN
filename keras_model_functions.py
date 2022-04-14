9d3741fac5cb7f4d9da8798dd1d5e88036a7b2a0
zhymir thompson <dzhymir@gmail.com>
2022-04-14 16:13:09 -0400

    Line wrap cleanup

--- keras_model_functions.py
+++ keras_model_functions.py
@@ -13,8 +13,11 @@ from pyts.image import RecurrencePlot
 # from pyts.datasets import load_gunpoint
 
 
-def plot_recurrence(y_values, rp, single_sample=True, show=False, save=False, save_name=''):
-    x_rp = rp.fit_transform(y_values.reshape(1, -1) if single_sample else y_values.reshape(-1, 1))
+def plot_recurrence(
+        y_values, rp, single_sample=True, show=False, save=False,
+        save_name=None):
+    x_rp = rp.fit_transform(
+        y_values.reshape(1, -1) if single_sample else y_values.reshape(-1, 1))
     x_rec = x_rp[0]
     if show:
         # Show the results for the first time series
@@ -22,14 +25,15 @@ def plot_recurrence(y_values, rp, single_sample=True, show=False, save=False, sa
         plt.imshow(x_rec, cmap='binary', origin='lower')
         plt.title('Recurrence Plot', fontsize=16)
         plt.tight_layout()
-        if save and save_name != '':
+        if save and save_name is not None and save_name != '':
             plt.savefig(save_name)
         if show:
             plt.show()
 
 
 def get_recurrence(y_values, rp, single_sample=True):
-    x_rp = rp.fit_transform(y_values.reshape(1, -1) if single_sample else y_values.reshape(-1, 1))
+    x_rp = rp.fit_transform(
+        y_values.reshape(1, -1) if single_sample else y_values.reshape(-1, 1))
     x_rec = x_rp[0]
     return x_rec
 
