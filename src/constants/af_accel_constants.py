894f253caefacad6cfae3e9413599b4e2937ba29
Zhymir Thompson <dzhymir@gmail.com>
2023-03-29 16:24:31 -0400

    Made constants file to reduce magic values.

---
+++ src/constants/af_accel_constants.py
@@ -0,0 +1,24 @@
+STANDARD_MODE = 'standard'
+EBGAN_MODE = 'ebgan'
+VAE_MODE = 'vae'
+
+NoNeedNoHave = 0
+NoNeedHave = 1
+NeedNoHave = 2
+NeedHave = 3
+
+CONFIG_MODE = 'training_mode'
+CONFIG_DATA = 'data'
+H_PARAMS = 'model_hyperparameters'
+
+DATA_DIR = 'dir'
+DATA_PATH = 'data'
+TIME_PATH = 'time'
+LABEL_PATH = 'labels'
+
+LATENT_DIM = 'latent_dimension'
+EPOCHS = 'epochs'
+BATCH_SIZE = 'batch_size'
+
+DTYPE = 'data_type'
+DATA_LENGTH = 5_000  # This should never change for this data, so keep constant
